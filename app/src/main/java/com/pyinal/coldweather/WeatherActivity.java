69fdb24957a630c4064a8b464acb267bfad7291e
pyinapl <1003115140@qq.com>
2018-06-11 15:36:31 +0800

    nav_Button and refresh

--- app/src/main/java/com/pyinal/coldweather/WeatherActivity.java
+++ app/src/main/java/com/pyinal/coldweather/WeatherActivity.java
@@ -3,10 +3,14 @@ package com.pyinal.coldweather;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -27,6 +31,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    private Button navButton;
+    public SwipeRefreshLayout swipeRefresh;
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -51,6 +59,10 @@ public class WeatherActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_weather);
 
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        navButton = (Button)findViewById(R.id.nav_button);
+        swipeRefresh = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
         weatherLayout = (ScrollView)findViewById(R.id.weather_layout);
         titleCity = (TextView)findViewById(R.id.title_city);
@@ -71,14 +83,22 @@ public class WeatherActivity extends AppCompatActivity {
             loadBingPic();
         }
         String weatherString = prefs.getString("weather",null);
+        final String weatherId;
         if (weatherString != null){
             Weather weather = Utility.handleWeatherResponnse(weatherString);
+            weatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         }else {
-            String weatherId = getIntent().getStringExtra("weather_id");
+            weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
         }
+        navButton.setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+        swipeRefresh.setOnRefreshListener(()->{
+            requestWeather(weatherId);
+        });
     }
 
     public void requestWeather(final String weatherId){
@@ -91,6 +111,7 @@ public class WeatherActivity extends AppCompatActivity {
                 e.printStackTrace();
                 runOnUiThread(()->{
                     Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_LONG).show();
+                    swipeRefresh.setRefreshing(false);
                 });
             }
 
@@ -107,6 +128,7 @@ public class WeatherActivity extends AppCompatActivity {
                     }else {
                         Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_LONG).show();
                     }
+                    swipeRefresh.setRefreshing(false);
                 });
             }
         });
@@ -139,9 +161,9 @@ public class WeatherActivity extends AppCompatActivity {
             pm25Text.setText(weather.aqi.city.pm25);
 
         }
-        String comfort = "舒适度"+weather.suggestion.comfort.info;
-        String carWash = "洗车指数"+weather.suggestion.carWash.info;
-        String sport = "运动建议"+weather.suggestion.sport.info;
+        String comfort = "舒适度:"+weather.suggestion.comfort.info;
+        String carWash = "洗车指数:"+weather.suggestion.carWash.info;
+        String sport = "运动建议:"+weather.suggestion.sport.info;
         comfortText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
